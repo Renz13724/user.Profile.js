5edc565ec1721c86a296176cbf56e0034a165a0a
VALDEZ R. <renzv753@gmail.com>
2024-10-30 17:20:00 +0800

    Create CS2C_MTExam_VALDEZ R.js

---
+++ CS2C_MTExam_VALDEZ R.js
@@ -0,0 +1,9 @@
+let fullName = "Renz Valdez";
+let age = 19;
+let favoriteNumber = 13;
+let favoriteColor = "Blue";
+
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
